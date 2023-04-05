e4d36fd115e88f819e928d0a5924748c6a675a42
MicahD18 <micah.daise@nwktc.edu>
2023-04-05 16:39:31 -0600

    added select all functionality

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -5,6 +5,7 @@ export interface EmployeeData {
   name: string;
   email: string;
   role: string;
+  isSelected: boolean;
 }
 @Component({
   selector: 'app-search-bar',
@@ -16,16 +17,16 @@ export class SearchBarComponent implements OnInit {
   searchText: string = "";
 
   employeeData: EmployeeData[] = [
-    {position: 1, name: 'Aaron Miles', email: "sample@gmail.com", role: 'Member'},
-    {position: 2, name: 'Sarah Potter', email: "sample@gmail.com", role: 'Member'},
-    {position: 3, name: 'Jim McClain', email: "sample@gmail.com", role: 'Admin'},
-    {position: 4, name: 'Caterina Binotto', email: "sample@gmail.com", role: 'Member'},
-    {position: 5, name: 'Arvind Kumar', email: "sample@gmail.com", role: 'Admin'},
-    {position: 6, name: 'Jeremy Skrdlant', email: "sample@gmail.com", role: 'Admin'},
-    {position: 7, name: 'Micah Daise', email: "sample@gmail.com", role: 'Member'},
-    {position: 8, name: 'Morgan Pritchard', email: "sample@gmail.com", role: 'Member'},
-    {position: 9, name: 'Bryce TerHaar', email: "sample@gmail.com", role: 'Admin'},
-    {position: 10, name: 'Michael Vars', email: "sample@gmail.com", role: 'Member'},
+    {position: 1, name: 'Aaron Miles', email: "sample@gmail.com", role: 'Member', isSelected: false },
+    {position: 2, name: 'Sarah Potter', email: "sample@gmail.com", role: 'Member', isSelected: false},
+    {position: 3, name: 'Jim McClain', email: "sample@gmail.com", role: 'Admin', isSelected: false},
+    {position: 4, name: 'Caterina Binotto', email: "sample@gmail.com", role: 'Member', isSelected: false},
+    {position: 5, name: 'Arvind Kumar', email: "sample@gmail.com", role: 'Admin', isSelected: false},
+    {position: 6, name: 'Jeremy Skrdlant', email: "sample@gmail.com", role: 'Admin', isSelected: false},
+    {position: 7, name: 'Micah Daise', email: "sample@gmail.com", role: 'Member', isSelected: false},
+    {position: 8, name: 'Morgan Pritchard', email: "sample@gmail.com", role: 'Member', isSelected: false},
+    {position: 9, name: 'Bryce TerHaar', email: "sample@gmail.com", role: 'Admin', isSelected: false},
+    {position: 10, name: 'Michael Vars', email: "sample@gmail.com", role: 'Member', isSelected: false},
   ];
 
   constructor() { }
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -3,28 +3,39 @@ import { EmployeeData } from '../search-bar/search-bar.component';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
-
   @Input() employeeData?: EmployeeData[];
   @Input() searchText!: string;
 
-  constructor() { }
+  allSelected: boolean = false;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
   displayedColumns: string[] = ['name'];
   dataSource: any = this.employeeData;
 
   deleteUser(index: number) {
     console.log(index);
-    this.employeeData?.map((item:any) => {
-      
+    this.employeeData?.map((item: any) => {
       if (index === item.position) {
         this.employeeData?.splice(index, 1);
       }
-    })
+    });
+  }
+
+  selectAll(selected: boolean) {
+    this.allSelected = selected;
+
+    if (this.employeeData == null) {
+      return;
+    }
+    this.employeeData.forEach((item: any) => {
+      // set all isSelected properties to true
+      item.isSelected = selected;
+    });
   }
 }
